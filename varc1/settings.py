0b3bdda561d24534fc681582215630f8bb5c1407
vishalraj@github <50707709+vishal-raj15@users.noreply.github.com>
2019-09-07 19:40:11 +0530

    Update settings.py

--- varc1/settings.py
+++ varc1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sc!kw7p7!2a@mc=y%ncvvmt=$8_rd%dpy+ia_1d_=ms+f9fvv@'
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
